73b39f6b3e478e3ee416efbaf0b136211d7bb8c6
Reuben George <reuben_george1998@outlook.com>
2021-12-07 15:51:36 +0000

    cardlist-done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Card.scss";
 
-const Card = ({imgUrl, name, abv, ibu, ebc, tagline}) => {
-  return (
-    <div className="cardlist__card">
-      <img src={imgUrl} alt="beer card" className="cardlist__card-image" />
-      <h2 className="cardlist__card-header">{name.toUpperCase()}
+const Card = ({imgUrl, name, abv, ibu, ebc, tagline, description, foodPairing}) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleClick = () => {
+    setShowInfo(!showInfo)
+  }
+  
+  
+
+  const cardJSX = (
+    <>
+      <img src={imgUrl} alt="beer card" className="card__image" />
+      <h2 className="card__header">{name.toUpperCase()}
       </h2>
-      <section className="cardlist__card-info">
-        <p className="heading">ABV</p>
-        <p className="heading">IBU</p>
-        <p className="heading">EBC</p>
-        <p className="info">{abv}%</p>
-        <p className="info">{ibu}</p>
-        <p className="info">{ebc}</p>
+      <section className="card__info">
+        <p className="card__alc-heading">ABV</p>
+        <p className="card__alc-heading">IBU</p>
+        <p className="card__alc-heading">EBC</p>
+        <p className="card__alc-info">{abv}%</p>
+        <p className="card__alc-info">{ibu}</p>
+        <p className="card__alc-info">{ebc}</p>
       </section>
-      <h4 className="cardlist__card-subheader">{tagline}</h4>
+      <h4 className="card__tagline">{tagline}</h4>
+    </>
+  );
+
+  const cardInfoJSX = (
+    <div className="card__back">
+      <h3 className="card__foodPair">Food pairings:</h3>
+      <ul className="card__foodPair-text">
+        {foodPairing.map((pair, index) => {
+          return <li key={index}>{pair}</li>
+        })}
+      </ul>
+      <h3 className="card__description">Description:</h3>
+      <p className="card__description-text">{description}</p>
+    </div>
+  );
+
+  return (
+    <div className="card" onClick={handleClick}>
+      {showInfo ? cardInfoJSX : cardJSX}
     </div>
   )
 }
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -5,7 +5,7 @@ import Card from "../Card/Card";
 
 const CardList = ({beerArray}) => {
   const cardJSX = beerArray.map((beer) => {
-    return <Card key={beer.id} name={beer.name} abv={beer.abv} ibu={beer.ibu} ebc={beer.ebc} tagline={beer.tagline} imgUrl={beer.image_url} />
+    return <Card key={beer.id} name={beer.name} abv={beer.abv} ibu={beer.ibu} ebc={beer.ebc} tagline={beer.tagline} imgUrl={beer.image_url} description={beer.description} foodPairing={beer.food_pairing} />
   });
 
   return (
